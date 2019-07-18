df6e382d12d147d3e57b4fe8be5f129b51ceca4c
Frank Luongo <frankluongo92@gmail.com>
2019-07-18 08:52:48 -0600

    Using Helpers to remove logic from templates

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Collabfield
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    # Do not load all helpers in all views
+    config.action_controller.include_all_helpers = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
