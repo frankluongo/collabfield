d01fa91d066732bb213a22b8b292da8b57c14d55
Frank Luongo <frankluongo92@gmail.com>
2019-07-22 13:16:22 -0600

    Built out search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,4 +25,21 @@ class PostsController < ApplicationController
   def get_posts
     Post.limit(30)
   end
+
+  def get_posts
+    branch = params[:action]
+    search = params[:search]
+    category = params[:category]
+
+    if category.blank? && search.blank?
+      posts = Post.by_branch(branch).all
+    elsif category.blank? && search.present?
+      posts = Post.by_branch(branch).search(search)
+    elsif category.present? && search.blank?
+      posts = Post.by_category(branch, category)
+    elsif category.present? && search.present?
+      posts = Post.by_category(branch, category).search(search)
+    else
+    end
+  end
 end
